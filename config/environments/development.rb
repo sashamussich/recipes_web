9eb8526fa5908e64b84666f3152db8aad16d13fb
sasha mussich <alexandra.muisch@gmail.com>
2015-12-05 00:23:32 -0200

    only left undone add user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # for unicorn server
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get(
+    ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
+  )
+
 end
